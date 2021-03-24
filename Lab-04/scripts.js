8e10930b41e33fd479869e9e7f5719489444271d
Aariyan Ganesh <ariyantndl@hotmail.com>
2021-03-24 16:52:25 +0000

    responsive header

--- Lab-04/scripts.js
+++ Lab-04/scripts.js
@@ -1,3 +1,9 @@
 menuToggler.addEventListener('click', ev => {
   menu.classList.toggle('open');
 });
+
+for (const element of document.querySelectorAll ('nav a')){
+  element.addEventListener('click',ev => {
+    menuToggler.classList.remove('open');
+  });
+}
